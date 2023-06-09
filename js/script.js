7aea8b9e733c017ccf700a8ad82fc45d81bf27ff
Yanis Labbe <ylabbe@c3r12p2.42quebec.com>
2023-06-09 16:39:00 -0400

    test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-    fetch('json/data.json')
+    fetch('data.json')
         .then(response => response.json())
         .then(data => {
             const info1 = document.getElementById('info1');
@@ -19,26 +19,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
             if (data['add-to-contacts']) {
                 info2.style.display = 'flex';
-                const addToContactsLink = document.getElementById('add-to-contacts').querySelector('a');
-                addToContactsLink.textContent = 'Add To Contacts';
-                addToContactsLink.href = 'data:text/html;charset=UTF-8,' + encodeURIComponent(`
-                <!DOCTYPE html>
-                <html>
-                <head>
-                    <meta name="format-detection" content="telephone=no">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Add Contact</title>
-                </head>
-                <body>
-                    <a href="tel:${data['call-now']}">${data['call-now']}</a>
-                    <script>
-                        window.onload = function() {
-                            window.location.href = "contacts://";
-                        };
-                    </script>
-                </body>
-                </html>
-                `);
+                document.getElementById('add-to-contacts').textContent = data['add-to-contacts'];
             } else {
                 info2.style.display = 'none';
             }
@@ -65,7 +46,7 @@ window.addEventListener('DOMContentLoaded', () => {
             } else {
                 info5.style.display = 'none';
             }
-
+            
             if (data.instagram) {
                 info6.style.display = 'flex';
                 const instagramLink = document.getElementById('instagram').querySelector('a');
@@ -74,7 +55,7 @@ window.addEventListener('DOMContentLoaded', () => {
             } else {
                 info6.style.display = 'none';
             }
-
+            
             if (data.website) {
                 info7.style.display = 'flex';
                 const websiteLink = document.getElementById('website').querySelector('a');
@@ -83,7 +64,7 @@ window.addEventListener('DOMContentLoaded', () => {
             } else {
                 info7.style.display = 'none';
             }
-
+            
         })
         .catch(error => {
             console.error('Error fetching data:', error);
