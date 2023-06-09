c2124b3f9d01cbb4be757449b34f661e30db4cc3
Yanis Labbe <ylabbe@c3r12p2.42quebec.com>
2023-06-09 15:51:46 -0400

    delete-comments

--- update_data.php
+++ update_data.php
@@ -1,23 +1,17 @@
 <?php
-// Get the modified data from the request
 $modifiedData = json_decode(file_get_contents('php://input'), true);
 
-// Read the existing data from data.json
 $data = json_decode(file_get_contents('json/data.json'), true);
 
-// Update the data with the modified values
 foreach ($modifiedData as $key => $value) {
     if (isset($data[$key])) {
         $data[$key] = $value;
     }
 }
 
-// Save the updated data back to data.json
 file_put_contents('json/data.json', json_encode($data, JSON_PRETTY_PRINT));
 
-// Prepare the response data
 $response = array('success' => true, 'message' => 'Data updated successfully');
 
-// Send the response as JSON
 header('Content-Type: application/json');
 echo json_encode($response);
